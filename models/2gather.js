89bc68fdf910c9bb75da715f492d99b0c8572678
Androlo <androlo1980@gmail.com>
2015-02-12 22:33:03 +0100

    Integration

---
+++ models/2gather.js
@@ -0,0 +1,300 @@
+
+function TwoGather() {
+
+	handlers = {};
+	tgApi = new TwoGatherAPI();
+
+	// Used to handle all incoming requests. It is set as the callback for
+	// incominghttp requests. It parses the request URL into an object, then 
+	// pass that object into the appropriate sub handler (should one exist).
+	// Sub handlers needs to return a response object.
+	this.handle = function(httpReq) {
+		Println("Receiving");
+		var urlObj = network.parseUrl(httpReq);
+		// Error 400 bad request
+		if (urlObj.error !== "") {
+			return null;
+		}
+		var res = urlObj.path[0];
+		var hFunc = handlers[res];
+		// Return an error.
+		if (typeof (hFunc) !== "function") {
+			network.getHttpResponse(400,{},"Bad request: no resource named: " + urlObj.path[0] + ".");
+		}
+		var resp = hFunc(urlObj, httpReq);
+
+		Println("RESPONSE OBJECT: " + JSON.stringify(resp));
+
+		// Weak check, but this is clearly not a valid response.
+		if (typeof (resp) !== "object" || resp.Body === undefined
+				|| resp.Header === undefined || resp.Status === undefined) {
+			return null;
+		}
+		return resp;
+	}
+	
+	// Handle user requests. Warning! - Very sophisticated routing algorithm at work.
+	handlers.user = function(urlObj,httpReq){
+		var path = urlObj.path;
+		if(path.length === 0 || path > 4){
+			return network.getHttpResponse(400,{},"Bad request: invalid path.");
+		} else if (path.length == 1){
+			return doUser("", httpReq.Method, httpReq.Body);
+		} else if (path.length == 2){
+			return doUser(path[1], httpReq.Method, httpReq.Body);
+		} else if (path.length == 3){
+			if(path[2] === "videos"){
+				return doVideo("","",httpReq.Method,httpReq.Body);
+			} else if (path[2] === "subs"){
+				Println("Sub request");
+				return doSub("",httpReq.Method,httpReq.Body);
+			}
+		} else if (path.length == 4){
+			if(path[2] === "videos"){
+				return doVideo(path[1],path[3],httpReq.Method,httpReq.Body);
+			} else if (path[2] === "subs"){
+				return doSub(path[3],httpReq.Method,httpReq.Body);
+			}
+		} else {
+			return network.getHttpResponse(400,{},"Bad request: malformed URL");
+		}
+	}
+	
+	handlers.txs = function(urlObj, httpReq) {
+		Println("Getting tx");
+		if(httpReq.Method === "GET") {			
+			if(urlObj.path.length !== 2){
+				return network.getHttpResponse(400,{},"Bad request: invalid path.");
+			}
+			var result = tgApi.checkTx(urlObj.path[1]);
+			Printf("Got result of tx: %v\n",result);
+			if(result === 0){
+				return network.getHttpResponse(404,{},"Resource not found.");
+			}
+			return network.getHttpResponseJSON(result);
+		} else {
+			return network.getHttpResponse(400,{},"Bad request: method not supported (" + httpReq.Method + ")");
+		}
+	}
+
+	function doUser(username, method, body){
+		if (method === "GET"){
+			// This is a request to get user data.
+			Println("Getting user: " + username);
+			var user = tgApi.getUser(username);
+			Println("User gotten. Type is: " + typeof(user));
+			if (user === null){
+				return network.getHttpResponse(404,{},"Resource not found.");
+			}
+			return network.getHttpResponseJSON(user);
+		} else if (method === "POST"){
+			// We get username from the body here.
+			var nameObj = JSON.parse(body);
+			if (nameObj.user_name === undefined || nameObj.user_name === ""){
+				return network.getHttpResponse(400,{},"Bad request - malformed user name.");
+			}
+			var hash = tgApi.createAccount(nameObj.user_name);
+			if (hash === "0x0"){
+				return network.getHttpResponse(500,{},"Internal error - blockchain transaction not processed.");
+			}
+			// DEBUG
+			monk.Commit();
+			
+			return network.getHttpResponseJSON(hash);
+		} else if (method === "PATCH"){
+			var user = tgApi.getUser(username);
+			if (user === null){
+				return network.getHttpResponse(404,{},"Resource not found.");
+			}
+			var patches = JSON.parse(body);
+			if (patches[0] === undefined){
+				return network.getHttpResponse(400,{},"Bad request - patch data not an array.");
+			}
+			var txHashes = [];
+			for(var i = 0; i < patches.length; i++){
+				var patch = patches[i];
+				if (!isPatch(patch)){
+					return network.getHttpResponse(400,{},"Bad request - patch data malformed.");
+				}
+				if(patch.field === "btc_addr"){
+					if(typeof(patch.value) !== "string" ){
+						return network.getHttpResponse(400,{},"Bad request - patch data malformed.");
+					}
+					if(patch.op === "replace"){
+						txHashes[i] = tgApi.setBTC(patch.value);
+					} else {
+						txHashes[i] = tgApi.setBTC("0x0");
+					}
+				} else if (patch.field === "doug_perm"){
+					if(typeof(patch.value) !== "boolean" ){
+						return network.getHttpResponse(400,{},"Bad request - patch data malformed.");
+					}
+					if(patch.op === "replace"){
+						txHashes[i] = tgApi.setDOUGPerm(username, patch.value);
+					} else {
+						txHashes[i] = tgApi.setDOUGPerm(username, false);
+					}
+				} else if (patch.field === "blacklist_perm"){
+					if(typeof(patch.value) !== "boolean" ){
+						return network.getHttpResponse(400,{},"Bad request - patch data malformed.");
+					}
+					if(patch.op === "replace"){
+						txHashes[i] = tgApi.setBlacklistPerm(username, patch.value);
+					} else {
+						txHashes[i] = tgApi.setBlacklistPerm(username, false);
+					}
+				} else {
+					return network.getHttpResponse(400,{},"Bad request - no such field in user: " + patch.field);
+				}
+			}
+			// DEBUG
+			monk.Commit();
+			return network.getHttpResponseJSON(patches);
+		} else if (method === "DELETE"){
+			Println("DELETING USER: " + username);
+			var hash = tgApi.deleteAccount(username);
+			if (hash === "0x0"){
+				return network.getHttpResponse(500,{},"Internal error - blockchain transaction not processed.");
+			}
+			// DEBUG
+			monk.Commit();
+			return network.getHttpResponseJSON(hash);
+		} else {
+			return network.getHttpResponse(405,{},"Method not allowed: " + method);
+		}
+	}
+
+	function doVideo(username,videoId, method, body){
+		if (method === "GET"){
+			// This is a request to get user data.
+			Println("Getting video: " + username + "/" + videoId);
+			var video = tgApi.getVideoData(username,videoId);
+			if (video === null){
+				return network.getHttpResponse(404,{},"Resource not found.");
+			}
+			return network.getHttpResponseJSON(video);
+		} else if (method === "POST"){
+			// We get videoId from the body here.
+			var nameObj = JSON.parse(body);
+			if (nameObj.name === undefined || nameObj.name === ""){
+				return network.getHttpResponse(400,{},"Bad request - malformed video owner name.");
+			}
+			if (nameObj.url === undefined || nameObj.url === ""){
+				return network.getHttpResponse(400,{},"Bad request - malformed video owner url.");
+			}
+			Println("Posting video: " + nameObj.name + "/" + nameObj.url);
+			var hash = tgApi.addVideo(nameObj.name,nameObj.url);
+
+			if (hash === "0x0"){
+				return network.getHttpResponse(500,{},"Internal error - blockchain transaction not processed.");
+			}
+			// DEBUG
+			monk.Commit();
+			return network.getHttpResponseJSON(hash);
+		} else if (method === "PATCH"){
+			var video = tgApi.getVideoData(username,videoId);
+			if (IsZero(video)){
+				return network.getHttpResponse(404,{},"Resource not found.");
+			}
+			var patches = JSON.parse(body);
+			if (patches[0] === undefined){
+				return network.getHttpResponse(400,{},"Bad request - patch data must be a non-empty array.");
+			}
+			var txHashes = [];
+			Println("ITERATING OVER PATCHES");
+			for(var i = 0; i < patches.length; i++){
+				var patch = patches[i];
+				if (!isPatch(patch)){
+					return network.getHttpResponse(400,{},"Bad request - patch data malformed.");
+				}
+				if(typeof(patch.value) !== "boolean" ){
+					return network.getHttpResponse(400,{},"Bad request - patch data malformed.");
+				}
+				if(patch.field === "blacklist"){
+					if(patch.op === "replace"){
+						txHashes[i] = tgApi.blacklistById(username, videoId);
+					} else {
+						return network.getHttpResponse(501,{},"Not yet supported - Blacklisting can't be undone.");	
+					}
+				} else if (patch.field === "flag"){
+					if(patch.op === "replace"){
+						txHashes[i] = tgApi.flagVid(username, videoId);
+					} else {
+						txHashes[i] = tgApi.clearFlagById(username, videoId);
+					}
+				} else {
+					return network.getHttpResponse(400,{},"Bad request - no such field in user: " + patch.field);
+				}
+			}
+			// DEBUG
+			monk.Commit();
+			return network.getHttpResponseJSON(patches);
+		} else if (method === "DELETE"){
+			var hash = tgApi.removeVideo(videoId);
+			if (hash === "0x0"){
+				return network.getHttpResponse(500,{},"Internal error - blockchain transaction not processed.");
+			}
+			// DEBUG
+			monk.Commit();
+			return network.getHttpResponseJSON(hash);
+		} else {
+			return network.getHttpResponse(405,{},"Method not allowed: " + method);
+		}
+	}
+
+	function doSub(username, method, body){
+		if (method === "POST"){
+			// We get username from the body here.
+			var nameObj = JSON.parse(body);
+			if (nameObj.user_name === undefined || nameObj.user_name === ""){
+				return network.getHttpResponse(400,{},"Bad request - malformed sub name.");
+			}
+			var hash = tgApi.subTo(nameObj.user_name);
+
+			if (hash === "0x0"){
+				return network.getHttpResponse(500,{},"Internal error - blockchain transaction not processed.");
+			}
+			// DEBUG
+			monk.Commit();
+
+			return network.getHttpResponseJSON(hash);
+		} else if (method === "DELETE"){
+			var hash = tgApi.unSubTo(username);
+
+			if (hash === "0x0"){
+				return network.getHttpResponse(500,{},"Internal error - blockchain transaction not processed.");
+			}
+
+			// DEBUG
+			monk.Commit();
+			return network.getHttpResponseJSON(hash);
+		} else {
+			return network.getHttpResponse(405,{},"Method not allowed: " + method);
+		}
+	}
+
+	function isPatch(obj){
+		if(obj.op === undefined || obj.field === undefined || obj.value === undefined){
+			return false;
+		} 
+		if (typeof(obj.op) !== "string" || (obj.op !== "replace" && obj.op !== "remove") ){
+			return false;
+		}
+		return true;
+	}
+
+	// Init
+	this.init = function(){
+		tgApi.init();
+	}
+
+};
+
+
+// *************************************** Initialization ***************************************
+
+var tg = new TwoGather();
+tg.init();
+
+network.registerIncomingHttpCallback(tg.handle);
+Println("2gather initialized");
---
+++ models/2gatherAPI.js
@@ -0,0 +1,561 @@
+function TwoGatherAPI() {
+	
+	var dougAddr = RootContract;
+	var flAddr = "";
+	var afAddr = "";
+	var myaccAddr = "";
+	var mysubs = [];
+	var myMonkAddr = "";
+	
+	var currentVid = null;
+	
+	var vidNum = 10;
+
+	// Used to keep track of account create and delete transactions.
+	var accTx = ""; 
+
+	// ****************************** TX tracking *****************************
+
+	var txData = {};
+	
+	// Codes for polling
+	// 0 - non existing
+	// 1 - pending
+	// 2 - failed
+	// 3 - succeeded
+	this.checkTx = function(txHash){
+		var txObj = txData[txHash];
+		if(txObj === undefined){
+			return 0;
+		}
+		// failed
+		else if (txObj.status === 2){
+			delete txData[txHash];
+			return 2;
+		} else if (txObj.status === 3){
+			// Tx was processed, but don't return 3, first run the test to see if the
+			// update it was expected to do actually happened.
+			delete txData[txHash];
+			return 3;
+		} else {
+			// Pending
+			return 1;
+		}
+	}
+		
+	this.newTxObj = function(){
+		return {"status" : 0 };
+	}
+		
+	this.statUpdatePre = function(event){
+		var txObj = txData[event.Resource.Hash];
+		if(txObj !== undefined){
+			txObj.status = 1;
+		}
+	}
+		
+	this.statUpdatePost = function(event){
+		var hash = "0x" + event.Resource.Hash;
+		Println("Post tx received: " + hash);
+		if(accTx !== ""){
+			if(hash === accTx){
+				myaccAddr = esl.kv.Value(afAddr, StringToHex("accounts"), myMonkAddr);
+				Println("Updating myaccAddr: " + myaccAddr);
+				accTx = "";
+			}
+		}
+		var txObj = txData[hash];
+		if(txObj !== undefined){
+			txObj.status = 3;
+		}
+		
+	}
+	
+	this.statUpdateFail = function(event){
+		var txObj = txData[event.Resource.Hash];
+		if(txObj !== undefined){
+			txObj.status = 2;
+		}
+	}
+	
+	// functions for adding new type of txs
+	this.trackTx = function(txHash){
+		Println("Added hash for tracking: " + txHash.slice(0,6) + "...");
+		var txObj = this.newTxObj();
+		txData[txHash] = txObj;
+		Println("Added tx data");
+	}
+	
+	// TODO all the others
+	
+	// ************************** Main functions *****************************
+	
+	// USER
+	
+	// Account Creation and removal Functions
+	// Create a new account
+	this.createAccount = function(userName) {
+		var txData = [];
+		txData.push("create");
+		txData.push(userName);
+		var hash = sendMsg(afAddr, txData);
+		accTx = hash;
+		this.trackTx(hash);
+		return hash;
+	}
+	
+	// This Deletes an account both from the usernames database and the actual
+	// contract.
+	this.deleteAccount = function(username) {
+		var channelAddr = usernameToChannelAddr(username);
+		var txData = [];
+		txData.push("deleteaccount");
+		var hash = sendMsg(channelAddr, txData);
+		this.trackTx(hash);
+		accTx = hash;
+		return hash;
+	}
+	
+	// Get the user data for a given user.
+	this.getUser = function(userName){
+		Println("Getting user");
+		var channelAddr = usernameToChannelAddr(userName);
+		Println("Channel address: " + channelAddr);
+		if (IsZero(channelAddr)){
+			Println("User not found");
+			return null;
+		}
+		
+		var userData = {};
+		
+		var chanData = getChanInfo(channelAddr);
+		var vids = getChanVids(channelAddr);
+		
+		userData.user_name = chanData.username;
+		userData.user_id = chanData.owner;
+		userData.created = chanData.created;
+		userData.btc_address = chanData.btc_address;
+		userData.doug_perm = hasDOUGPerm(userName);
+		userData.blacklist_perm = hasBlacklistPerm(userName);
+		userData.videos = vids;
+		
+		var subs = [];
+		for(var i = 0; i < mysubs.length; i++){
+			subs[i] = channelAddrToUsername(mysubs[i]);
+		}
+		userData.subscriptions = subs;
+		return userData;
+	}
+	
+	// Subscribe to a channel
+	this.subTo = function(userName) {
+		var channelAddr = usernameToChannelAddr(userName);
+		var txData = [];
+		txData.push("subscribe");
+		txData.push(channelAddr);
+		mysubs.push(channelAddr);
+		var hash = sendMsg(myaccAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+	
+	// unsubscribe from a channel
+	this.unSubTo = function(userName) {
+		var channelAddr = usernameToChannelAddr(userName);
+		var txData = [];
+		txData.push("unsubscribe");
+		txData.push(channelAddr);
+		var i = mysubs.indexOf(channelAddr);
+		mysubs.splice(i, 1);
+		var hash = sendMsg(myaccAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+	
+	// My Account Functions
+	this.setBTC = function(btcaddress) {
+		var txData = [];
+		txData.push("setBTC");
+		txData.push(btcaddress);
+		var hash = sendMsg(myaccAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+
+	// Grant a user doug permissions. Can only be done if you have that 
+	// permissions.
+	this.setDOUGPerm = function(username, on) {
+		var pubAddr = usernameToAddr(username);
+		var txData = [];
+		txData.push("setperm");
+		txData.push("doug");
+		txData.push(pubAddr);
+		if(on){
+			txData.push("0x1");
+		} else {
+			txData.push("0x0");
+		}
+		var hash = sendMsg(dougAddr, txData);
+		this.trackTx(hash);
+		return hash;		
+	}
+
+	this.setBlacklistPerm = function(username, on) {
+		var pubAddr = usernameToAddr(username);
+		var txData = [];
+		txData.push("setperm");
+		txData.push("blacklist");
+		txData.push(pubAddr);
+		if(on){
+			txData.push("0x1");
+		} else {
+			txData.push("0x0");
+		}
+		var hash = sendMsg(dougAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+
+	// VIDEO
+
+	// Post a video
+	this.addVideo = function(name,url) {
+		var hash = writeFile(url);
+		if(hash === ""){
+			Println("Error when adding file '" + url + "' to ipfs.");
+			return "0x0";
+		}
+		return this.postVid(name,hash);
+	}
+
+	// Remove a video
+	this.removeVideo = function(vidnum) {
+		var txData = [];
+		txData.push("remove");
+		txData.push(vidnum);
+		var hash = sendMsg(myaccAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+	
+	// Get a video
+	this.getVideoData = function(username, vidnum) {
+		channelAddr = usernameToChannelAddr(username);
+		var vids = esl.ll.GetPairsRev(channelAddr, StringToHex("uploads"), 10);
+		for (var i = 0; i < vids.length; i++) {
+			if(Equals(vids[i].Key,vidnum)){
+				var vdat = {};
+				vdat.name = esl.kv.Value(channelAddr, StringToHex("vidnames"),
+						vids[i].Key);
+				vdat.file = vids[i].Value;
+				vdat.date = esl.kv.Value(channelAddr, StringToHex("uploaddate"),
+						vids[i].Key);
+				vdat.vidnum = vids[i].Key;
+				vdat.status = esl.kv.Value(channelAddr, StringToHex("status"),
+						vids[i].Key);
+				return vdat;
+			}
+		}
+		return null;
+	}
+
+	// Get a video that is flagged
+	this.getFlaggedVid = function(casenum) {
+		
+		var vdat = {};
+		vdat.account = esl.ll.Main(flAddr,StringToHex("flaggedaddr"),casenum);
+		vdat.vidnum = esl.kv.Value(flAddr,StringToHex("flaggedvidn"),casenum);
+		vdat.casenum = casenum;
+
+		return vdat;
+	}
+
+	this.getCasenumById = function(username,vidnum){
+		Println("Get casenum by ID: " + username + "/" + vidnum);
+		var channelAddr = usernameToChannelAddr(username);
+		var fVids = this.getFlaggedVids(10);
+		Printf("Number of flagged vids: %d\n", fVids.length);
+		for(var i = 0; i < fVids.length; i++){
+			var fVid = fVids[i];
+			Println("Channel address: " + channelAddr);
+			Println("FVID ACCOUNT: " + fVid.account);
+			Println("FVID VIDNUM: " + fVid.vidnum);
+			Println("FVID CASENUM: " + fVid.casenum);
+			if(Equals(fVid.account,channelAddr)){
+				if(Equals(fVid.vidnum,vidnum)){
+					return fVid.casenum;
+				}
+			}
+		}
+		return "0x0";
+	}
+
+	this.getFlaggedVidById = function(username,vidnum) {
+		var casenum = this.getCasenumById(username,vidnum);
+		if(Equals(casenum,"0x0")){
+			return "0x0";
+		}
+		return this.getFlaggedVid(casenum);
+	}
+
+	this.flagVid = function(username, vidnum) {
+		Println("FLAGGING VID: " + username + "/" + vidnum);
+		var channelAddr = usernameToChannelAddr(username);
+		var txData = [];
+		txData.push("flag");
+		txData.push(vidnum);
+		var hash = sendMsg(channelAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+
+	// clear the flag off a video
+	this.clearFlagById = function(username,vidnum) {
+		Println("Clear flags by ID: " + username + "/" + vidnum);
+		var casenum = this.getCasenumById(username,vidnum);
+		Println("CaseNum: " + casenum);
+		if(Equals(casenum,"0x0")){
+			Println("Casenum is 0");
+			return "0x0";
+		}
+		return this.clearFlag(casenum);
+	}
+
+	// clear the flag off a video
+	this.clearFlag = function(casenum) {
+		var txData = [];
+		txData.push("rmflag");
+		txData.push(casenum);
+		var hash = sendMsg(flAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+
+	this.blacklistById = function(username,vidnum){
+		Println("Blacklist by ID: " + username + "/" + vidnum);
+		var casenum = this.getCasenumById(username,vidnum);
+		Println("CaseNum: " + casenum);
+		if(Equals(casenum,"0x0")){
+			Println("Casenum is 0");
+			return "0x0";
+		}
+		return this.blacklist(casenum);
+	}
+
+	// Accept an offer. Both params are strings.
+	this.blacklist = function(casenum) {
+		var txData = [];
+		txData.push("blacklist");
+		txData.push(casenum);
+		var hash = sendMsg(flAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+
+	
+	// ************************ Helpers ***************************
+	
+	// Takes a username and gets the address, if any.
+	function usernameToAddr(username){
+		return esl.ll.Main(afAddr,sutil.stringToHex("usernames"), sutil.stringToHex(username));
+	};
+	
+	// Takes a user name and returns the channel address, if any.
+	function usernameToChannelAddr(username){
+		var pubAddr = esl.ll.Main(afAddr,sutil.stringToHex("usernames"),sutil.stringToHex(username));
+		return esl.kv.Value(afAddr,sutil.stringToHex("accounts"),pubAddr);
+	};
+	
+	function channelAddrToUsername(channelAddr){
+		return sutil.hexToString(esl.single.Value(channelAddr, sutil.stringToHex("username")));
+	};
+	
+	// For getting the videos associated with a channel. Passing your account
+	// address will get your own videos
+	function getChanVids(channelAddr) {
+		// This Returns the "num" most recent videos for the channel at
+		// channelAddr
+		// If num is zero then returns the full list
+
+		var vids = esl.ll.GetPairsRev(channelAddr, StringToHex("uploads"), 10);
+
+		var ret = [];
+		for (var i = 0; i < vids.length; i++) {
+			var vdat = {};
+			vdat.name = esl.kv.Value(channelAddr, StringToHex("vidnames"),
+					vids[i].Key);
+			vdat.file = vids[i].Value;
+			vdat.date = esl.kv.Value(channelAddr, StringToHex("uploaddate"),
+					vids[i].Key);
+			vdat.vidnum = vids[i].Key;
+			vdat.status = esl.kv.Value(channelAddr, StringToHex("status"),
+					vids[i].Key);
+			ret.push(vdat);
+		}
+		return ret;
+	};
+
+	// Get Information about an account
+	function getChanInfo(channelAddr) {
+		var ret = {};
+		ret.owner = esl.single.Value(channelAddr, StringToHex("owner"));
+		ret.username = esl.single.Value(channelAddr, StringToHex("username"));
+		ret.created = esl.single.Value(channelAddr, StringToHex("created"));
+		ret.btc_address = esl.single.Value(channelAddr, StringToHex("BTCAddr"));
+		return ret;
+	}
+	
+	// Check if a user has doug permissions.
+	function hasDOUGPerm(username){
+		
+		var pubAddr = usernameToAddr(username);
+		var DOUGIndex = esl.ll.Main(dougAddr,StringToHex("permnames"),StringToHex("doug"));
+		var Permval = esl.array.Element(dougAddr,StringToHex("perms"),pubAddr,DOUGIndex);
+		if (Equals(Permval, "0x1")){
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	// Check if a user has video blacklisting permissions.
+	function hasBlacklistPerm(username){
+		
+		var pubAddr = usernameToAddr(username);
+		var BlacklistIndex = esl.ll.Main(dougAddr,StringToHex("permnames"),StringToHex("blacklist"));
+		var Permval = esl.array.Element(dougAddr,StringToHex("perms"),pubAddr,BlacklistIndex);
+		if (Equals(Permval, "0x1")){
+			return true;
+		} else {
+			return false;
+		}
+		
+	};
+	
+	// Channel obtaining functions
+	function getAllAccounts() {
+		
+		var allacc = esl.ll.GetPairs(afAddr, StringToHex("usernames"), 0);
+		var ret = [];
+		for (var i = 0; i < allacc.length; i++) {
+			var accdat = {};
+			accdat.username = allacc[i].Value;
+			accdat.pubAddr = allacc[i].Key;
+			accdat.chanAddr = esl.kv.Value(afAddr, StringToHex("accounts"),
+					allacc[i].Key);
+			ret.push(accdat);
+		}
+		return ret;
+		
+	};
+
+	// Find an account
+	/*
+	this.findAccount = function(username) {
+		var accPubAddr = esl.ll
+				.Main(afAddr, StringToHex("usernames"), username);
+		if (accPubAddr == 0) {
+			return nil;
+		} else {
+			return esl.kv.Value(afAddr, StringToHex("accounts"), accPubAddr);
+		}
+	}
+	*/
+
+	// ADMINISTRATIVE FUNCTIONS
+
+	// Get the flagged vids
+	this.getFlaggedVids = function(num) {
+		// This Returns the "num" most recent flagged videos
+		// If num is zero then returns the full list
+		var vids = esl.ll.GetPairsRev(flAddr, StringToHex("flaggedaddr"), num);
+
+		var ret = [];
+		for (var i = 0; i < vids.length; i++) {
+			var vdat = {};
+			vdat.account = vids[i].Value;
+			vdat.vidnum = esl.kv.Value(flAddr, StringToHex("flaggedvidn"),
+					vids[i].Key);
+			vdat.casenum = vids[i].Key;
+			ret.push(vdat);
+		}
+		return ret;
+	};
+
+	
+	// Run state updates THIS PRELOADS VIDEOS FROM YOUR SUBSCRIBERS SO YOU DON"T
+	// HAVE TO WAIT
+	/*
+	this.sync = function() {
+		var vidsofsubs = [];
+
+		for (var i = 0; i < mysubs.length; i++) {
+			var chan = mysubs[i];
+			var tvids = GetChanVids(mysubs[i], loadnum);
+			for (var j; j < tvids.length; j++) {
+				// For each video we find we need to check its status
+				if (tvids[j].status == 5) {
+					// Then It is blacklisted Remove this from IPFS
+					// note when blacklisted you must match against the first 14
+					// Bytes
+				} else {
+					GetFile(tvids[j].file);
+				}
+			}
+		}
+	}
+	*/
+
+	// Post a video
+	this.postVid = function(name, hash) {
+		var txData = [];
+		txData.push("upload");
+		// This is the correct order.
+		txData.push(hash);
+		txData.push(name);
+		var hash = sendMsg(myaccAddr, txData);
+		this.trackTx(hash);
+		return hash;
+	}
+
+	// No websockets running here, meaning there's only one possible sub from this runtime.
+	this.sub = function() {
+		events.subscribe("monk","newTx:pre","", this.statUpdatePre, "does_not_matter");
+		events.subscribe("monk","newTx:post","", this.statUpdatePost, "does_not_matter");
+		events.subscribe("monk","newTx:pre:fail","", this.statUpdateFail, "does_not_matter");
+		events.subscribe("monk","newTx:post:fail","", this.statUpdateFail, "does_not_matter");
+	}
+	
+	// No websockets running here, meaning there's only one possible sub from this runtime.
+	function unsub(){
+		events.unsubscribe("monk","newTx:pre","does_not_matter");
+		events.unsubscribe("monk","newTx:post","does_not_matter");
+		events.unsubscribe("monk","newTx:pre:fail","does_not_matter");
+		events.unsubscribe("monk","newTx:post:fail","does_not_matter");
+	}
+
+	// This needs to be run when the dapp is started.
+	this.init = function() {
+		Println("Initializing 2gather");
+		// Start subscribing to tx events.
+		this.sub();
+		Println("DOUG address: " + dougAddr);
+		// Find relevant contracts from Doug
+		flAddr = esl.ll.Main(dougAddr, StringToHex("DOUG"),
+				StringToHex("flaggedlist"));
+		Println("flAddr: " + flAddr);
+		afAddr = esl.ll.Main(dougAddr, StringToHex("DOUG"),
+				StringToHex("accountfactory"));
+
+		Println("afAddr: " + afAddr);
+
+		myMonkAddr = "0x" + monk.ActiveAddress().Data;
+		Println("My address: " + myMonkAddr);
+		// Find Account contract associated with that name
+		myaccAddr = esl.kv.Value(afAddr, StringToHex("accounts"), myMonkAddr);
+		Println("My Account address: " + myaccAddr);
+		// Load subs (This will be used for auto distribution of files)
+		mysubs = esl.ll.GetList(myaccAddr, StringToHex("subs"));
+		Printf("mysubs %v\n",mysubs);
+	}
+};
